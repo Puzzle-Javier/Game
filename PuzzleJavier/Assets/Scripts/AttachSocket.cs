579a8d807a37b3838984faf8712943606988af05
H José Caviedes <hjcaviedesb@unal.edu.co>
2019-12-31 15:09:39 -0500

    First local commit

---
+++ PuzzleJavier/Assets/Scripts/AttachSocket.cs
@@ -0,0 +1,274 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+using System.Text;
+
+public class AttachSocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Transform child;
+    private Transform parent;
+    private Transform child_1;
+    private Transform parent_1;
+    private MovePiece script1;
+    private MovePiece script2;
+    private Transform parent_angle1;
+    private Transform parent_angle2;
+    private float angle1;
+    private float angle2;
+
+    // public float dif;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        string string1 = null;
+        string string2 = null;
+
+        script1 = collider.transform.parent.gameObject.GetComponent<MovePiece>();
+        script2 = this.transform.parent.gameObject.GetComponent<MovePiece>();
+
+        if (collider is CircleCollider2D && collider.gameObject.name == gameObject.name)
+        {
+
+            string1 = collider.transform.parent.name;
+            string2 = gameObject.transform.parent.name;
+            //float angle1 = collider.transform.parent.transform.localEulerAngles.z;
+            //float angle2 = this.transform.parent.transform.localEulerAngles.z;
+
+            parent_angle1 = searchParent(collider.transform.parent);
+            parent_angle2 = searchParent(this.transform.parent);
+
+            angle1 = parent_angle1.localEulerAngles.z;
+            angle2 = parent_angle2.localEulerAngles.z;
+
+            print(this.transform.parent.name + " angle " + angle2);
+            print(collider.transform.parent.name + " angle " + angle1);
+
+            float angle = Mathf.Round(angle1);
+          
+            if (Mathf.Abs(angle1 - angle2) < 5)
+            {
+                if (script2.pieceStatus == "pickedup")
+                {
+                    child = collider.transform.parent;
+                    parent = this.gameObject.transform.parent;
+                    alignPieces(parent, child, string1, string2,angle);
+                    transformParent(parent, child);
+                    collider.enabled = false;
+                    this.enabled = false;
+                }
+                else if (script1.pieceStatus == "pickedup")
+                {
+                    child = this.transform.parent;
+                    parent = collider.transform.parent;
+                    alignPieces(parent, child, string1, string2,angle);
+                    transformParent(parent, child);
+                    collider.enabled = false;
+                    this.enabled = false;
+                }
+            }
+        }
+    }
+
+
+    void transformParent(Transform parent,Transform child)
+    {
+        if(child.transform.parent.name == "Pieces")
+        {
+            child.transform.SetParent(parent.transform);
+            //child.transform.localPosition = Vector3.zero;
+            //child.transform.localRotation = Quaternion.identity;
+            return;
+        }
+        transformParent(parent, child.transform.parent);
+    }
+
+
+    Transform searchParent(Transform root)
+    {
+        if(root.transform.parent.name == "Pieces")
+        {
+            return root.transform;
+        }
+        searchParent(root.transform.parent);
+        return root.transform.parent;
+    }
+
+    void alignPieces(Transform parent, Transform child, string name1, string name2, float angle)
+    {
+        char[] string1 = name1.ToCharArray();
+        char[] string2 = name2.ToCharArray();
+        float dif = 3.42f;
+        float row_x, row_y,column_x,column_y;
+        float angleRad = angle * Mathf.Deg2Rad;
+
+        row_x = dif * Mathf.Cos(angleRad);
+        row_y = dif * Mathf.Sin(angleRad);
+        column_y = dif * Mathf.Cos(angleRad);
+        column_x = dif * Mathf.Sin(angleRad);
+
+        if(name1[0] == name2[0])
+        {
+            if(angle < 90)
+            {
+                movePieceXA(parent,child,row_x,row_y);
+            }
+            else if (angle > 90 && angle < 270)
+            {
+                movePieceXB(parent,child,row_x,row_y);
+            }
+            else if(angle > 270 && angle < 360)
+            {
+                movePieceXA(parent,child,row_x,row_y);
+            }
+            else if(angle == 90)
+            {
+                movePieceXC(parent,child,row_x,row_y);
+            }
+            else if(angle == 270)
+            {
+                movePieceXD(parent,child,row_x,row_y);
+            }
+        }
+        else 
+        {
+            if(angle > 0 && angle < 180 && angle != 90)
+            {
+                movePieceYA(parent, child, row_x, row_y);
+            }
+            else if(angle > 180 && angle < 360 && angle != 270)
+            {
+                movePieceYB(parent,child,row_x,row_y);
+            }
+            else if(angle == 270)
+            {
+                movePieceYC(parent,child,column_x);
+            }
+            else if(angle == 90)
+            {
+                movePieceYD(parent, child, column_x);
+            }
+            else if (angle == 180)
+            {
+                movePieceYE(parent,child,column_y);
+            }
+            else
+            {
+                movePieceYF(parent,child,column_y);
+            }
+        }
+    }
+
+
+    void movePieceXA(Transform parent, Transform child,float x,float y){
+
+        if (parent.transform.position.x < child.transform.position.x){
+            parent.transform.position = new Vector3(child.transform.position.x - x, child.transform.position.y - y, child.transform.position.z);
+        }
+        else{
+            parent.transform.position = new Vector3(child.transform.position.x + x, child.transform.position.y + y, child.transform.position.z);
+        }
+    }
+
+    void movePieceXB(Transform parent, Transform child, float x, float y){
+
+        if (parent.transform.position.x < child.transform.position.x){
+            parent.transform.position = new Vector3(child.transform.position.x + x, child.transform.position.y + y, child.transform.position.z);
+        }
+        else{
+            parent.transform.position = new Vector3(child.transform.position.x - x, child.transform.position.y - y, child.transform.position.z);
+        }  
+    }
+
+    void movePieceXC(Transform parent, Transform child, float x, float y){
+
+        if (parent.transform.position.y < child.transform.position.y){
+            parent.transform.position = new Vector3(child.transform.position.x, child.transform.position.y - y, child.transform.position.z);
+        }
+        else{
+            parent.transform.position = new Vector3(child.transform.position.x, child.transform.position.y + y, child.transform.position.z);
+        }
+    }
+
+    void movePieceXD(Transform parent, Transform child, float x, float y){
+
+        if (parent.transform.position.y < child.transform.position.y){
+            parent.transform.position = new Vector3(child.transform.position.x, child.transform.position.y + y, child.transform.position.z);
+        }
+        else{
+            parent.transform.position = new Vector3(child.transform.position.x, child.transform.position.y - y, child.transform.position.z);
+        }
+    }
+
+
+    void movePieceYA(Transform parent, Transform child, float x, float y){
+        if (parent.transform.position.y < child.transform.position.y){
+            parent.transform.position = new Vector3(child.transform.position.x + x, child.transform.position.y - y, child.transform.position.z);
+        }
+        else{
+            parent.transform.position = new Vector3(child.transform.position.x - x, child.transform.position.y + y, child.transform.position.z);
+        }
+    }
+
+    void movePieceYB(Transform parent, Transform child, float x, float y){
+        if (parent.transform.position.y < child.transform.position.y){
+            parent.transform.position = new Vector3(child.transform.position.x - x, child.transform.position.y + y, child.transform.position.z);
+        }
+        else{
+            parent.transform.position = new Vector3(child.transform.position.x + x, child.transform.position.y - y, child.transform.position.z);
+        }
+    }
+
+    void movePieceYC(Transform parent, Transform child, float x){
+    
+        if (parent.transform.position.x < child.transform.position.x){
+            parent.transform.position = new Vector3(child.transform.position.x + x, child.transform.position.y, child.transform.position.z);
+        }
+        else{
+            parent.transform.position = new Vector3(child.transform.position.x - x, child.transform.position.y, child.transform.position.z);
+        }
+    }
+
+    void movePieceYD(Transform parent, Transform child, float x){
+    
+        if (parent.transform.position.x < child.transform.position.x){
+            parent.transform.position = new Vector3(child.transform.position.x - x, child.transform.position.y, child.transform.position.z);
+        }
+        else{
+            parent.transform.position = new Vector3(child.transform.position.x + x, child.transform.position.y, child.transform.position.z);
+        }
+    }
+
+    void movePieceYE(Transform parent,Transform child,float y){
+    
+        if (parent.transform.position.y < child.transform.position.y){
+            parent.transform.position = new Vector3(child.transform.position.x, child.transform.position.y + y, child.transform.position.z);
+        }
+        else{
+            parent.transform.position = new Vector3(child.transform.position.x, child.transform.position.y - y, child.transform.position.z);
+        }
+    }
+
+    void movePieceYF(Transform parent,Transform child, float y){
+    
+        if (parent.transform.position.y < child.transform.position.y){
+            parent.transform.position = new Vector3(child.transform.position.x, child.transform.position.y - y, child.transform.position.z);
+        }
+        else{
+            parent.transform.position = new Vector3(child.transform.position.x, child.transform.position.y + y, child.transform.position.z);
+        }
+    }
+}
---
+++ PuzzleJavier/Assets/Scripts/MovePiece.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePiece : MonoBehaviour
+{
+
+    public string pieceStatus = "idle";
+    public bool moving = false;
+    private MovePiece script1;
+    private string status;
+    private bool selected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pieceStatus == "pickedup")
+        {
+            moving = true;
+            selected = true;
+            Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = objPosition;
+        }
+        else
+        {
+            selected = false;
+        }
+
+    }
+
+    //Detect 2D collision 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.name == gameObject.name)
+        {
+            transform.position = other.gameObject.transform.position;
+            pieceStatus = "locked";
+            moving = false;
+        }
+    }
+
+    //Action taken when a mouse button is pressed
+    void OnMouseDown()
+    {
+        if (pieceStatus == "idle")
+        {
+            checkParent(this.gameObject.transform,this.gameObject.transform.parent);
+            pieceStatus = "pickedup";
+            status = "childPicked";
+            traverseChildren(this.gameObject.transform,status);
+
+        }
+        else
+        {
+            pieceStatus = "idle";
+            traverseChildren(this.gameObject.transform, pieceStatus);
+        }
+    }
+
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1) && selected)
+        {
+            Rotate();
+        }
+    }
+
+
+    void Rotate()
+    {
+        int degrees = 45;
+        Vector3 to = new Vector3(0, 0, degrees);
+        //transform.eulerAngles = Vector3.Lerp(this.transform.rotation.eulerAngles, to);
+        transform.Rotate(0, 0, degrees);
+    }
+
+    //Method to chek parent and change transform
+    void checkParent(Transform root, Transform parent)
+    {
+        if(parent.name == "Pieces")
+        {
+            return;
+        }
+
+        root.transform.SetParent(parent.transform.parent.transform);
+        parent.transform.SetParent(root.transform);
+        checkParent(root,parent.transform.parent.transform);
+    }
+
+    void traverseChildren(Transform root, string status)
+    {
+        foreach (Transform child in root)
+        {
+            // Do something with child, then recurse.
+
+            script1 = child.gameObject.GetComponent<MovePiece>();
+            if(script1 != null)
+            {
+                script1.pieceStatus = status;
+                traverseChildren(child, status);
+            }
+
+        }
+    }
+
+}
